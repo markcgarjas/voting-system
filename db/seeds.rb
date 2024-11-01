b58c7b4523fb8f4ac6989555b868da46c29143fd
mark <markcarlo859@gmail.com>
2024-11-01 17:41:37 +0800

    Add organization and member data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+organizations = [
+  { name: 'University of Students Government', code: 'USG' },
+  { name: 'Science and Engineering Society', code: 'SES' },
+  { name: 'Business Leaders Association', code: 'BLA' },
+  { name: 'Environmental Awareness Club', code: 'EAC' },
+  { name: 'International Students Association', code: 'ISA' },
+  { name: 'Arts and Literature Society', code: 'ALS' },
+  { name: 'Sports and Recreation Council', code: 'SRC' },
+  { name: 'Technology and Innovation Club', code: 'TIC' },
+  { name: 'Community Outreach Program', code: 'COP' },
+  { name: 'Debate and Public Speaking Society', code: 'DPSS' },
+  { name: 'Health and Wellness Group', code: 'HWG' }
+]
+
+organizations.each do |org_attrs|
+  Organization.find_or_create_by(org_attrs)
+end
+
+puts "Seeded #{Organization.count} organizations."
+
+10.times do
+  Member.find_or_create_by(user: User.student.sample, organization: Organization.all.sample, role: :member)
+end
+
+puts "Seeded #{Member.count} members."
