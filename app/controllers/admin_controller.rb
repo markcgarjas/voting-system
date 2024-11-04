43e9510bc611ec216ca23988d0ba5fbe334beb32
mark <markcarlo859@gmail.com>
2024-11-04 15:23:28 +0800

    Modify the button to small and add sort in some page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ActionController::Base
+  include ApplicationHelper
+
   before_action :authenticate_admin_user!
   layout 'admin'
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def sorted_organization_members(members)
+    members.sort_by { |member| member.officer_position.sort }
+  end
+
+  def sorted_students(students)
+    students.sort_by { |user| user.username }
+  end
+
+  def sorted_positions(positions)
+    positions.sort_by { |position| position.name }
+  end
 end
