b88c897b086278b89e9a5151a4660b5b5cf2ec33
mark <markcarlo859@gmail.com>
2024-11-06 14:59:06 +0800

    Adjust admin layouts, and styles

---
+++ app/helpers/admin/sidebar_helper.rb
@@ -0,0 +1,14 @@
+module Admin::SidebarHelper
+  MENU_LISTS = {
+    student: %w[
+      students organizations
+    ],
+    admin: %w[
+      admins
+    ]
+  }
+
+  def menu_switch(name, controller, menu_list = Admin::SidebarHelper::MENU_LISTS)
+    menu_list[name].include?(controller) ? '' : 'hidden'
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,20 @@ module ApplicationHelper
   def sorted_positions(positions)
     positions.sort_by { |position| position.name }
   end
+
+  def content_title
+    if current_page?(admin_root_path)
+      'Admin Dashboard'
+    else
+      "#{action_name == 'index' ? '' : action_name.titleize} #{controller_name.titleize}"
+    end
+  end
+
+  def is_active(controller)
+    if controller_name == controller
+      'bg-blue-500 text-white'
+    else
+      'hover:bg-gray-700'
+    end
+  end
 end
