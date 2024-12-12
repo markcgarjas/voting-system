eb0fce06af0d01b4b6c1446a170a23f40b33eaf5
mark <markcarlo859@gmail.com>
2024-12-12 13:37:45 +0800

    Set some tailwind css to class using apply

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend: {
+      colors: {},
+      fontFamily: {
+        roboto: ['Roboto', 'serif'],
+      },
+      animation: {
+        'border-pulse': 'borderPulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'fade-in': 'fadeIn 0.3s ease-out'
+      },
+      keyframes: {
+        borderPulse: {
+          '0%, 100%': { borderColor: 'rgba(209, 213, 219, 1)' },
+          '50%': { borderColor: 'rgba(209, 213, 219, 0.5)' }
+        },
+        fadeIn: {
+          'from': {
+            opacity: '0',
+            transform: 'translateY(20px)'
+          },
+          'to': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        }
+      }
+    }
+  },
   content: [
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
