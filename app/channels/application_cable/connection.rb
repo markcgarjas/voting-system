8dad34575a5a30caa75e4c6e1ff76a895f223843
markcgarjas <markcarlo859@gmail.com>
2025-02-01 00:46:08 +0800

    Improve ActionCable connection authentication and make maintainable and robust

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -11,13 +11,33 @@ module ApplicationCable
     private
 
     def find_verified_user
-      user_scope = Devise.mappings.map(&:first).detect { |scope| cookies.signed["#{scope}.id"].present? }
-      verified_user = User.find_by(id: cookies.signed["#{user_scope}.id"])
-      if verified_user && cookies.signed["#{user_scope}.expires_at"] > Time.current
-        verified_user
-      else
-        reject_unauthorized_connection
-      end
+      return reject_unauthorized_connection unless valid_user_session?
+
+      user_scope = find_user_scope
+      return reject_unauthorized_connection unless user_scope
+
+      authenticate_user(user_scope)
+    end
+
+    def find_user_scope
+      Devise.mappings.map(&:first).find { |scope| cookies.signed["#{scope}.id"].present? }
+    end
+
+    def valid_user_session?
+      cookies.signed.present?
+    end
+
+    def authenticate_user(user_scope)
+      user_id = cookies.signed["#{user_scope}.id"]
+      expiration = cookies.signed["#{user_scope}.expires_at"]
+
+      return reject_unauthorized_connection unless valid_expiration?(expiration)
+
+      User.find_by(id: user_id) || reject_unauthorized_connection
+    end
+
+    def valid_expiration?(expiration)
+      expiration.present? && expiration > Time.current
     end
   end
 end
